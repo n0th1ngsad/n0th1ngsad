465171f4dc3408f015d4b4b3929f6950f4174fb7
n0th1ng <desychristanti1@gmail.com>
2024-07-08 23:27:07 +0700

    Update script.js

--- Assets/script.js
+++ Assets/script.js
@@ -5,9 +5,8 @@ function typeWriter() {
     if (index < text.length) {
         document.getElementById("typewriter-text").innerHTML += text.charAt(index);
         index++;
-        setTimeout(typeWriter, 100); // Adjust typing speed here
+        setTimeout(typeWriter, 100); 
     } else {
-        // Blinking cursor effect after typing
         document.getElementById("typewriter-text").style.borderRight = '0.1em solid black';
         setInterval(() => {
             document.getElementById("typewriter-text").style.borderRight = (document.getElementById("typewriter-text").style.borderRight == '0.1em solid black' ? 'none' : '0.1em solid black');
